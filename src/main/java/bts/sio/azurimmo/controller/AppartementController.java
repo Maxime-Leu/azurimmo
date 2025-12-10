fe38a15ff2ae45ac8b008c8453685f2a8fee7fe3
Maxime <leu.maxime27@gmail.com>
2025-12-10 10:42:25 +0100

    Ajout d'une methode getAppartements afin de tous les récupérer. Et ajout de la documentation swagger

--- src/main/java/bts/sio/azurimmo/controller/AppartementController.java
+++ src/main/java/bts/sio/azurimmo/controller/AppartementController.java
@@ -2,17 +2,26 @@ package bts.sio.azurimmo.controller;
 
 import bts.sio.azurimmo.model.Appartement;
 import bts.sio.azurimmo.service.AppartementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/appartements")
+@Tag(name = "Appartements", description = "Gestion des appartements")
 public class AppartementController {
 	
    @Autowired
     private AppartementService appartementService;
-
+   
+    @GetMapping
+	@Operation(summary = "Récupère tous les appartements")
+	public  List<Appartement> getAppartements() {
+	        return appartementService.getAppartements();
+	}
 	
     @PostMapping("/")
     public Appartement createAppartement(@RequestBody Appartement appartement) {
--- src/main/java/bts/sio/azurimmo/service/AppartementService.java
+++ src/main/java/bts/sio/azurimmo/service/AppartementService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import bts.sio.azurimmo.model.Appartement;
+import bts.sio.azurimmo.model.dto.AppartementDTO;
+import bts.sio.azurimmo.model.dto.BatimentDTO;
 import bts.sio.azurimmo.repository.AppartementRepository;
 import lombok.Data;
 
@@ -32,5 +34,10 @@ public class AppartementService {
 	public List<Appartement> getAppartementsSuperieur(float minSurface) {
 		    return appartementRepository.findBySurfaceGreaterThan(minSurface);
 	}
+	
+	public List<Appartement> getAppartements() {
+        return appartementRepository.findAll();
+	}
 
+	
 }
